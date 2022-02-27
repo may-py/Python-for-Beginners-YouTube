19d3883ca8f0803b7b3b3be99c758973109257aa
Mayur Suthar <65947362+may-py@users.noreply.github.com>
2022-02-27 15:22:41 +0700

    initial

---
+++ 11 while loop in Python.py
@@ -0,0 +1,40 @@
+# while True:break
+
+# print('Anybody Can Code')
+# print('Anybody Can Code')
+# print('Anybody Can Code')
+# print('Anybody Can Code')
+# print('Anybody Can Code')
+
+# counter variable
+# x = 1
+# while x < 10:
+#     print('Anybody Can Code with While Loop',x)
+#     x = x + 1
+#     if x == 5:
+#         break
+
+
+
+
+i = 1
+while i <= 10:
+    print('Anybody Can Code',end=" ")
+    j = 1
+    while j <= 3:
+        print("Python", end=" ")
+        j = j + 1
+    i = i + 1
+    print()
+
+
+
+
+# initialize total and number
+# total = 0
+# z = 1
+# while z <= 10:
+#     total = total + z
+#     z = z+1
+# print("The sum is", total)
+# print(1+2+3+4+5+6+7+8+9+10)
