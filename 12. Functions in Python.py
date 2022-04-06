9f12c78238c64670f64f9c09d5c84752c116c9b7
Mayur Suthar <65947362+may-py@users.noreply.github.com>
2022-04-06 15:08:05 +0700

    initial

---
+++ 12. Functions in Python.py
@@ -0,0 +1,77 @@
+# structure
+# def add():
+#     # print('Hello World')
+#     pass
+
+#
+# Print statement
+# def func():
+#     print('Hello World1')
+# # #
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+# func()
+# func()
+# func()
+# func()
+# func()
+
+
+
+# def func1(a):
+#     print(a)
+
+# func1()
+#
+#
+# func1("Hello ABCC")
+# func1("Hello India")
+
+# Perform calculation
+# def func1(a,b):
+#     print(a*b)
+#     print(a+b)
+#     print(a-b)
+#     print(a/b)
+#
+# func1(10,5)
+
+
+# def func1(a="ABCC"):
+#     print('Hello', a)
+#
+# func1()
+# a = input("Enter username:")
+# func1(a)
+#
+#
+#
+#
+# #
+# def args(*name):
+#     print(name)
+# #
+# args(1,2,3,4,5)
+# #
+# def func3(*name):
+#     for n in name:
+#         print(n)
+# # #
+# func3('Love','and','Like','Coding')
+#
+# def func4(a,b,c):
+#     print(a,b,c)
+# #
+# args = ('Hello','World','Coding')
+# func4(*args)
+#
+#
+# def func5(**name):
+#     print(name)
+# #
+# #
+# func5(a=1,b=2,c=3,d=4)
+# #
