9c8580639bb731b23d424ea8ed9371000c075fa0
Mayur Suthar <65947362+may-py@users.noreply.github.com>
2022-04-22 09:41:33 +0700

    Initial

---
+++ 13. File Handling.py
@@ -0,0 +1,66 @@
+"""
+create = x
+write = w
+read = r
+append = a
+read+write = r+
+write+read = w+
+"""
+
+# testc = open('testc.txt','x')
+# testc.close()
+
+# testr = open('testc.txt','r')
+# print(testr.read())
+# testr.write('Hello World!')
+# testr.close()
+
+# testw = open('testw.txt','w')
+# testw.write('1Hello ABCC!\n')
+# testw.write('2Hello ABCC!\n')
+# testw.write('3Hello ABCC!\n')
+# testw.write('4Hello ABCC!\n')
+# testw.write('5Hello ABCC!\n')
+# # print(testw.read())
+# testw.close()
+
+# test = open('testw.txt','r')
+# print(test.read())
+# test.write('Happy Learnig!')
+# print(test.tell())
+# print(test.read())
+# test.seek(0)
+# print(test.read())
+# print("Try",test.read())
+# test.seek(0)
+# print('Read line',test.readline())
+# print('Read line',test.readline())
+# test.seek(0)
+# print('Read lines',test.readlines())
+# test.close()
+
+
+# open('testrw.txt','x').close()
+# testrw = open('testrw.txt','r+')
+# # testrw.write('Hello ABCC')
+# print(testrw.read())
+# testrw.write('I am updating this file with r+ mode \n')
+# testrw.write('I am updating this file with r+ mode \n')
+# print(testrw.read())
+# testrw.close()
+
+
+# testwr = open('testwr.txt', 'w+')
+# # print(testwr.read())
+# testwr.write('I am writing this file with w+ mode')
+# print(testwr.tell())
+# print(testwr.read())
+# testwr.seek(0)
+# print(testwr.read())
+# testwr.close()
+
+
+# testa = open('testrw.txt','a')
+# print(testa.tell())
+# testa.write('Hello world')
+# print(testa.tell())
