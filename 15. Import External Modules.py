8d0f6f379497dc3e9627e27bbc82e2e7eca4ac25
Mayur Suthar <65947362+may-py@users.noreply.github.com>
2022-06-26 16:04:59 +0700

    initial

---
+++ 15. Import External Modules.py
@@ -0,0 +1,16 @@
+help('modules')
+
+
+
+import random
+import io
+import time
+import math
+import datetime
+#
+print(math.pi)
+print(math.sqrt(25))
+
+import numpy
+import pandas as pd
+import matplotlib as mlp
